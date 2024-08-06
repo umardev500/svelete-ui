067a45faaf485a4c55bd7857089c775f578e45ff
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-08-06 17:53:55 +0700

    rename import

--- src/components/molecules/sidebar/SidebarMenu.svelte
+++ src/components/molecules/sidebar/SidebarMenu.svelte
@@ -1,30 +1,16 @@
 <script lang="ts">
-	import SidebarMenuPosSub from '@components/molecules/sidebar/SidebarMenuPosSub.svelte';
+	import SidebarMenuList from '@components/molecules/sidebar/SidebarMenuList.svelte';
 	import type { SidebarMenuType } from '@typed/sidebarMenu';
-	import Sortable from 'sortablejs';
 	import { onMount } from 'svelte';
 
 	export let menus: SidebarMenuType[] = [];
 	let menuElement: HTMLElement;
 
-	onMount(() => {
-		Sortable.create(menuElement, {
-			animation: 150,
-			onEnd: (e) => {
-				const item = e.item;
-				item.classList.remove('border-t');
-			},
-			onMove: (e) => {
-				const dragged = e.dragged;
-				const trigger = dragged.firstElementChild;
-				dragged.classList.add('border-t');
-			}
-		});
-	});
+	onMount(() => {});
 </script>
 
 <ul bind:this={menuElement}>
 	{#each menus as menu}
-		<SidebarMenuPosSub {menu} hasSubmenu={!!menu.submenu} />
+		<SidebarMenuList {menu} hasSubmenu={!!menu.submenu} />
 	{/each}
 </ul>
