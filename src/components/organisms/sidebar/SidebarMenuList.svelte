5ddc36103a980fe310df6852554247ac5a101720
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-08-08 09:06:03 +0700

    add class check

--- src/components/organisms/sidebar/SidebarMenuList.svelte
+++ src/components/organisms/sidebar/SidebarMenuList.svelte
@@ -49,7 +49,9 @@
 					// Handle if children of submenu is added to the root
 					const item = e.item;
 					const itemAnchor = item.querySelector('a');
-					itemAnchor?.classList.remove('pl-6');
+					const itemAnchorContainPL = itemAnchor?.classList.contains('pl-6');
+
+					if (itemAnchorContainPL) itemAnchor?.classList.remove('pl-6');
 				}
 			};
 		}
