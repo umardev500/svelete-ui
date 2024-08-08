2a48bbc0c453da8ffbf74c05eb8d4975d25c90e3
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-08-09 01:03:24 +0700

    hide desktop menu on mobile

--- src/components/organisms/header/Header.svelte
+++ src/components/organisms/header/Header.svelte
@@ -20,13 +20,15 @@
 	class={sidebarOpen ? 'show' : ''}
 	style="--sidebar-width: {sidebarOpen ? '22.125rem' : '4.5rem'}"
 >
-	<nav class="px-4 h-16 border-b flex items-center justify-between">
-		<!-- Left menus -->
-		<HeaderMenuLeftList {items} />
-		<!-- End of left menus -->
-		<!-- Right Menus -->
-		<HeaderRightMenuList />
-		<!-- End of Right Menus -->
+	<nav class="px-4 h-16 border-b">
+		<div class="h-full flex items-center justify-between desktop-menu">
+			<!-- Left menus -->
+			<HeaderMenuLeftList {items} />
+			<!-- End of left menus -->
+			<!-- Right Menus -->
+			<HeaderRightMenuList />
+			<!-- End of Right Menus -->
+		</div>
 	</nav>
 </header>
 
@@ -36,8 +38,17 @@
 		margin-left: 0rem;
 		transition: margin-left var(--margin-transition-duration) var(--margin-timing-function);
 
+		.desktop-menu {
+			transform: translateY(-4rem);
+			transition: transform var(--margin-transition-duration) var(--margin-timing-function);
+		}
+
 		@media screen and (min-width: 1024px) {
 			margin-left: 4.5rem;
+
+			.desktop-menu {
+				transform: translateY(0);
+			}
 
 			&.show {
 				margin-left: 22.125rem;
