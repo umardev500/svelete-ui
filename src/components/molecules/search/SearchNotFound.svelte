3ead0410824fa2601fbb9fff4209f50263430edc
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-08-13 09:26:57 +0700

    add hidden search element

---
+++ src/components/molecules/search/SearchNotFound.svelte
@@ -0,0 +1,7 @@
+<div class="text-center py-6">
+	<h3 class="text-base font-medium text-gray-800">No result found</h3>
+	<p class="mt-2 text-base text-gray-600">
+		We are currently not able to find what you are looking for. Please try searching for something
+		else
+	</p>
+</div>
--- src/components/organisms/search/Search.svelte
+++ src/components/organisms/search/Search.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
 	import SearchInput from '@components/atoms/form/SearchInput.svelte';
 	import SearchListing from '@components/molecules/search/SearchListing.svelte';
+	import SearchNotFound from '@components/molecules/search/SearchNotFound.svelte';
 
 	let contentEl: HTMLElement;
+	let notFoundEl: HTMLElement;
+
 	const handleSearch = (e: CustomEvent<string>) => {
 		const value = e.detail;
 
-		if (value !== '') {
+		if (value !== '' && value !== '-') {
 			// do search
 			contentEl.classList.remove('hidden');
 		} else {
 			contentEl.classList.add('hidden');
+		}
+
+		if (value === '-') {
+			notFoundEl.classList.remove('hidden');
+		} else {
+			notFoundEl.classList.add('hidden');
 		}
 	};
 </script>
@@ -20,5 +29,9 @@
 
 	<div class="result hidden" bind:this={contentEl}>
 		<SearchListing />
+	</div>
+
+	<div class="mt-4 hidden" bind:this={notFoundEl}>
+		<SearchNotFound />
 	</div>
 </div>
